852f0200e09ec8e06af63636c64d42a71861015d
Drew Dingle <drew.dingle@gmail.com>
2025-08-06 11:26:45 -0700

    modifies the data beinf persisted from the get /products api call. - removes storing the product list - adds storing the current product it (currently efaulting to the first product in the returned array - updates the components propr to be 'productId={currentProductId}'

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,24 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Overview from './Overview.jsx';
+import Reviews from './Reviews.jsx';
 
 export default function App() {
-  const [products, setProducts] = useState(null);
+
+  const [currentProductId, setCurrentProductId] = useState(null);
 
   useEffect(() => {
     axios.get('/products')
-         .then(res => setProducts(res.data))
+         .then(res => setCurrentProductId(res.data[0].id))
          .catch(err => console.log('failed to get products', err));
   }, []);
 
-  if (products === null) {
+  if (currentProductId === null) {
     return (<div>loading...</div>);
   }
 
   return (
     <div className="app">
       Our App!!
-      <Overview product={products[0]}/>
+      <Overview productId={currentProductId}/>
+      <Reviews productId={currentProductId}/>
     </div>
   )
 };
