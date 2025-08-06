5c7f9bacf576f2403133e2c934097cd8e158a426
Eduardo Nava <ed.nava.valencia@gmail.com>
2025-08-06 10:11:18 -0700

    fix: moved api call to App and passed down props to Overview

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,18 @@
 import Overview from './Overview.jsx'
 
 export default function App() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    axios.get('/products')
+         .then(res => setProducts(res.data))
+         .catch(err => console.log('failed to get products', err));
+  }, []);
+
   return (
     <div className="app">
       Our App!!
-      <Overview/>
+      <Overview product={products[0]}/>
     </div>
   )
 };
--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,14 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Overview() {
-  const [product, setProduct] = useState(null);
-  const id = ''
-  useEffect(() => {
-    // axios.get(`/product/${id}`)
-    axios.get('/products')
-         .then(payload => setProduct(payload.data[0]))
-  }, [])
+function Overview({product}) {
   return (
     <div>
       Overview
