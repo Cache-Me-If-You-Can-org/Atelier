8d14a493f5958be085b357216e4f4dd1ad6c0b72
Eduardo Nava <ed.nava.valencia@gmail.com>
2025-08-11 19:54:32 -0700

    fix: updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
-  extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-  ],
-  env: {
-    browser: true,
-    es2021: true,
+  root: true,
+  extends: ["airbnb", "airbnb/hooks", "plugin:react/recommended", "plugin:jsx-a11y/recommended"],
+  env: { browser: true, es2021: true },
+  settings: { 
+    react: { version: "detect" },
+    "import/resolver": { node: { extensions: [".js", ".jsx"] } }
   },
-  settings: {
-    react: { version: 'detect' },
-  },
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  overrides: [
+    {
+      files: ["client/src/**/*.{js,jsx}"],
+      env: { browser: true, node: false },
+      rules: {
+        "no-console": ["warn", { allow: ["warn", "error", "debug"] }],
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+        "jsx-quotes": ["error", "prefer-single"],
+      }
+    }
+  ]
 };
--- client/src/components/overview/components/StyleSelector.jsx
+++ client/src/components/overview/components/StyleSelector.jsx
@@ -1,40 +1,47 @@
 import React from 'react';
-import Image from '../../shared/Image.jsx';
 import { Check } from '@phosphor-icons/react';
-import { getSkus, getQtys } from '../lib/helpers.js';
+import Image from '../../shared/Image';
+import { getSkus, getQtys } from '../lib/helpers';
 import * as g from '../../global.module.css';
 import * as css from '../styles/style_selector.module.css';
 
-function StyleSelector({styles, selectedStyle, setSelectedStyle, setSku, setQty, sku}) {
-
-  return(
+function StyleSelector({
+  styles, selectedStyle, setSelectedStyle, setSkuId, setQty, skuId,
+}) {
+  return (
     <>
       <div className={`${g.group} ${css.gap}`}>
-        <div><strong>STYLE {'>'}</strong></div>
+        <div>
+          <strong>
+            STYLE
+            {'>'}
+          </strong>
+        </div>
         <div>{styles[selectedStyle].name.toUpperCase()}</div>
       </div>
       <div className={css.grid}>
         {styles.map((style, i) => (
           <div key={style.style_id} className={css.circle}>
-            {selectedStyle === i &&
+            {selectedStyle === i
+              && (
               <div className={`${css.checkmark} ${g.center}`}>
-                <Check size={10}/>
+                <Check size={10} />
               </div>
-            }
+              )}
             <Image
               className={css.image}
               src={style.photos[0].thumbnail_url}
               onClick={() => {
-                setSku(getSkus(styles[i])[0])
-                setQty(getQtys(styles[selectedStyle], sku)[0])
-                setSelectedStyle(i)
+                setSkuId(getSkus(styles[i])[0]);
+                setQty(getQtys(styles[selectedStyle], skuId)[0]);
+                setSelectedStyle(i);
               }}
             />
           </div>
         ))}
       </div>
     </>
-  )
+  );
 }
 
 export default StyleSelector;
