c40c0a8ff759135ac7ffdbef0bd6cb1f9ec24415
Eduardo Nava <ed.nava.valencia@gmail.com>
2025-08-13 02:21:15 -0700

    feat: info footer component complete

---
+++ client/src/components/overview/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Check } from '@phosphor-icons/react';
+import * as g from '../../global.module.css';
+import * as css from '../styles/footer.module.css';
+
+function Footer({ product }) {
+  return (
+    <div className={g.container}>
+      <div className={[g.group, g.gapMd].join(' ')}>
+        <div className={[g.stack, g.gapSm].join(' ')}>
+          <div className={[g.textMd, g.bold].join(' ')}>
+            {product.slogan}
+          </div>
+          <div className={g.textSm}>
+            {product.description}
+          </div>
+        </div>
+        <div className={css.divider} />
+        <div className={[g.stack, css.features].join(' ')}>
+          {product.features.map((feature) => (
+            <div key={feature.value} className={[g.group, g.gapSm, css.featureRow].join(' ')}>
+              <Check size={20} className={css.icon} />
+              <span className={css.text}>{feature.value}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
