abaab97322809165c7dc2ea83f422bd89891233e
Benjamin Nicholson <robertsonorama@gmail.com>
2025-08-13 21:26:29 -0700

    creating post request functionality for add review

--- client/src/components/benRatingsAndReviews/BenRatingsAndReviews.jsx
+++ client/src/components/benRatingsAndReviews/BenRatingsAndReviews.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReviewsList from './reviews/ReviewsList';
 import Ratings from './ratings/Ratings';
+import ReviewsServices from './services/ReviewsServices';
 import * as styles from './reviews.module.css';
 
 function BenRatingsAndReviews({ productId }) {
+  const [meta, setMeta] = useState([]);
+  useEffect(() => {
+    ReviewsServices.getMeta(productId, (metaData) => {
+      setMeta(metaData);
+    });
+  }, [productId]);
   return (
     <div>
       <div className={styles.ratingsAndReviewsLayout}>
         <div className={styles.ratingsWrapper}>
-          <Ratings productId={productId} />
+          <Ratings productId={productId} meta={meta} />
         </div>
         <div className={styles.reviewsWrapper}>
-          <ReviewsList productId={productId} />
+          <ReviewsList productId={productId} meta={meta} />
         </div>
       </div>
     </div>
--- client/src/components/benRatingsAndReviews/reviews/addReview/CharacteristicInput.jsx
+++ client/src/components/benRatingsAndReviews/reviews/addReview/CharacteristicInput.jsx
@@ -3,33 +3,8 @@ import * as styles from '../../reviews.module.css';
 
 // Reusable component for a characteristic radio group
 function CharacteristicInput({
-  label, options, value, setValue, name,
+  label, options, value, setValue, name, id,
 }) {
-  let id = '';
-  switch (name) {
-    case 'size':
-      id = '000000';
-      break;
-    case 'width':
-      id = '000001';
-      break;
-    case 'comfort':
-      id = '125033';
-      break;
-    case 'quality':
-      id = '125034';
-      break;
-    case 'length':
-      id = '125032';
-      break;
-    case 'fit':
-      id = '125031';
-      break;
-    default:
-      id = '';
-      break;
-  }
-
   return (
     <div className={styles.characteristic}>
       <h3 className={styles.radioSectionTitle}>
