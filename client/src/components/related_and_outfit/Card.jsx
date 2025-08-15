55c356c485ffd9dc1d37c32fae1ed31cfbd9e58f
Benjamin Noble <tworedfrog2@gmail.com>
2025-08-15 14:11:49 -0700

    Outfit storing (#55)
    * added comparison table without css
    
    * changed Modal to handle a style prop to adjust the container
    
    * added proper css and values for the comparison table
    
    * cleaned up the syntax
    
    * added star system for displaying rating in shared
    
    * finished up linting
    
    * fixed ALL eslint erros
    
    * outfit storage started and eslint fixes
    
    * add products to outfit using js-cookie
    
    * remove button added for outfit cards
    
    * changed placeholder for card when not loaded

--- client/src/components/related_and_outfit/Card.jsx
+++ client/src/components/related_and_outfit/Card.jsx
@@ -1,11 +1,79 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import * as styles from './relatedOutfit.module.css';
-import ComparisonTable from './ComparisonTable.jsx';
-import Modal from '../shared/Modal.jsx';
-import QuarterStarRating from '../shared/QuarterStarRating.jsx';
+import ComparisonTable from './ComparisonTable';
+import Modal from '../shared/Modal';
+import QuarterStarRating from '../shared/QuarterStarRating';
 
-export default function Card({ productId, originalProductId }) {
+function ImageWithButton({
+  url,
+  related,
+  original,
+  remove,
+}) {
+  const [hover, setHover] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className='thumbnail-square' style={{ height: 200 }}>
+      <img className='thumbnail-image' src={url} alt={original.name} style={{ objectPosition: 'center bottom' }} />
+      {remove ? (
+        <button
+          type='button'
+          className={styles.overlayBtn}
+          onClick={() => remove()}
+        >
+          &times;
+        </button>
+      )
+        : (
+          <>
+            <button
+              type='button'
+              className={styles.overlayBtn}
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+              onClick={() => setIsOpen(true)}
+            >
+              {hover ? '★' : '☆'}
+            </button>
+            <Modal
+              isOpen={isOpen}
+              setIsOpen={setIsOpen}
+              Module={<ComparisonTable related={related} original={original} />}
+              style={{ width: '40%' }}
+            />
+          </>
+        )}
+    </div>
+  );
+}
+
+function calculateStars(ratings) {
+  // Puts the string amounts of each rating
+  // in a single place
+  const allRatings = [
+    ratings['1'],
+    ratings['2'],
+    ratings['3'],
+    ratings['4'],
+    ratings['5'],
+  ];
+  let total = 0;
+  let valueTotal = 0;
+
+  // Calculates the average using (total * value) / total
+  for (let i = 0; i < allRatings.length; i += 1) {
+    total += Number(allRatings[i]) * 1;
+    valueTotal += Number(allRatings[i]) * (i + 1);
+  }
+
+  return valueTotal / total;
+}
+
+// remove is used to see if the card is an outfit card
+// (if remove is a valid function then it's an outfit card)
+export default function Card({ productId, originalProductId, remove }) {
   const [product, setProduct] = useState(null);
   const [originalProduct, setOriginalProduct] = useState(null);
   const [productImages, setProductImages] = useState(null);
@@ -14,95 +82,56 @@ export default function Card({ productId, originalProductId }) {
   useEffect(() => {
     // Fetch basic data (price, name, category)
     axios.get(`/products/${productId}`)
-      .then(res => {
+      .then((res) => {
         setProduct(res.data);
         // Fetch review data for the time being
-        return axios.get(`/products/${originalProductId}`)
+        return axios.get(`/products/${originalProductId}`);
       })
-      .then(res => {
+      .then((res) => {
         setOriginalProduct(res.data);
         // Fetch review data for the time being
         return axios.get('/reviews/meta', {
-          params: {product_id: productId}
-        })
+          params: { product_id: productId },
+        });
       })
-      .then(res => {
-        setRatings(res.data.ratings)
+      .then((res) => {
+        setRatings(res.data.ratings);
         // Fetch images for slide show and thumbnail
-        return axios.get(`/products/${productId}/styles`)
+        return axios.get(`/products/${productId}/styles`);
       })
-      .then(res => setProductImages(res.data.results[0].photos))
-      .catch(err => {
-        console.error('error loading product by id:', err)
+      .then((res) => setProductImages(res.data.results[0].photos))
+      .catch((err) => {
+        console.error('error loading product by id:', err);
       });
-  }, [])
+  }, [originalProductId, productId]);
 
   if (!productImages) {
     return (
       <div className={styles.productCard}>
-        <p>Loading...</p>
+        <div className='thumbnail-square' style={{ height: 200 }}>
+          <img className='thumbnail-image' src='https://blocks.astratic.com/img/general-img-landscape.png' alt='loading...' style={{ objectPosition: 'center bottom' }} />
+        </div>
+        <div className={styles.productCardInfo}>
+          <small>LOADING...</small>
+        </div>
       </div>
-    )
+    );
   }
 
   // Get the star unicode for the footer portion of the card
-  var number = calculateStars(ratings);
+  const number = calculateStars(ratings);
 
   return (
     <div className={styles.productCard}>
-      <ImageWithButton url={productImages[0].thumbnail_url ? productImages[0].thumbnail_url : 'https://blocks.astratic.com/img/general-img-landscape.png'} related={product} original={originalProduct} />
+      <ImageWithButton url={productImages[0].thumbnail_url ? productImages[0].thumbnail_url : 'https://blocks.astratic.com/img/general-img-landscape.png'} related={product} original={originalProduct} remove={remove} />
       <div className={styles.productCardInfo}>
         <small>{product.category.toUpperCase()}</small>
         <h4>{product.name}</h4>
-        <small>${product.default_price}</small>
-        <QuarterStarRating rating={number}/>
+        <small>{product.default_price}</small>
+        <div className={styles.cardStars}>
+          <QuarterStarRating rating={number} />
+        </div>
       </div>
     </div>
-  )
-}
-
-function ImageWithButton({ url, related, original }) {
-  const [hover, setHover] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <div className="thumbnail-square" style={{height: 200}}>
-      <img className="thumbnail-image" src={url} style={{objectPosition: 'center bottom'}}/>
-      <button className={styles.overlayBtn}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        onClick={() => setIsOpen(true)}
-      >
-        {hover ? '★' : '☆'}
-      </button>
-      <Modal
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        Module={<ComparisonTable related={related} original={original} />}
-        style={{ width: "40%" }}
-      />
-    </div>
   );
-};
-
-function calculateStars(ratings) {
-  // Puts the string amounts of each rating
-  // in a single place
-  var allRatings = [
-    ratings['1'],
-    ratings['2'],
-    ratings['3'],
-    ratings['4'],
-    ratings['5']
-  ]
-
-  // Calculates the average using (total * value) / total
-  var total = 0;
-  var valueTotal = 0;
-  for (var i = 0; i < allRatings.length; i++) {
-    total += Number(allRatings[i]) * 1;
-    valueTotal += Number(allRatings[i]) * (i + 1);
-  }
-
-  return valueTotal / total;
 }
--- client/src/components/shared/Modal.jsx
+++ client/src/components/shared/Modal.jsx
@@ -1,7 +1,8 @@
-import { createPortal } from "react-dom";
+import React from 'react';
+import { createPortal } from 'react-dom';
 // This portal ^ allows the modal to go all the way out
 // to the root instead of being trapped in the DOM component it's in
-import * as shared from "./shared.module.css";
+import * as shared from './shared.module.css';
 
 // This Modal component will be passed in 3 different props:
 //  -isOpen and setIsOpen will be states that you will need to make
@@ -21,33 +22,42 @@ import * as shared from "./shared.module.css";
 // </div>
 // );
 
-// (For any more reference you can look at src/components/related_and_outfit/Card.jsx (function at line 56))
+// (For any more reference you can look at
+// src/components/related_and_outfit/Card.jsx (function at line 56))
 
 // This allows for custom buttons, custom text, and even imgs to be
 // passed in, opposed to having a set button that must be pressed
 // to open the modal by default
 
-// I've also added a custom "style" prop so you can adjust the container of the
+// I've also added a custom 'style' prop so you can adjust the container of the
 // modal as you wish to
 
-export default function Modal({ isOpen, setIsOpen, Module, style = {} }) {
+export default function Modal({
+  isOpen,
+  setIsOpen,
+  Module,
+  style = {},
+}) {
   if (!isOpen) return null;
 
   return createPortal(
     <div
-      id="activeModal"
+      id='activeModal'
       className={shared.modal}
-      onClick={(e) => {
-        if (e.target.id === "activeModal") setIsOpen(false);
-      }}
     >
-      <div className={shared["modal-content"]} style={style}>
-        <span className={shared.close} onClick={() => setIsOpen(false)}>
+      <div className={shared['modal-content']} style={style}>
+        <span
+          className={shared.close}
+          onKeyPress={() => {}}
+          onClick={() => setIsOpen(false)}
+          role='button'
+          tabIndex='0'
+        >
           &times;
         </span>
         {Module}
       </div>
     </div>,
-    document.getElementById("modal-root")
+    document.getElementById('modal-root'),
   );
 }
