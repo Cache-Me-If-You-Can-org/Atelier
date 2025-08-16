d4e37823413b8cbacaca30c6ed5550d537abeb80
Ruby Chang <rchang825@hotmail.com>
2025-08-15 23:26:16 -0700

    another test product id

--- client/src/components/QA/index.jsx
+++ client/src/components/QA/index.jsx
@@ -7,12 +7,13 @@ function QA({ product, setSelectedProductId }) {
   // const productId = currentProductId; // product from server
   // const productId = '37322'; // multiple answers, 1 q
   // const productId = '37324'; // multiple answers, multiple questions
-  function differentProduct() {
-    setSelectedProductId(37322);
+  function differentProduct(id) {
+    setSelectedProductId(id);
   }
   return (
     <section id='questionsAndAnswers'>
-      <input type='button' onClick={differentProduct} value='Get other product' />
+      <input type='button' onClick={() => { differentProduct(37322); }} value='Get another product' />
+      <input type='button' onClick={() => { differentProduct(37324); }} value='Get another other product' />
       <h4 className={[styles.qaTitle, g.textMd].join(' ')}>QUESTIONS & ANSWERS</h4>
       <QuestionsList product={product} />
     </section>
