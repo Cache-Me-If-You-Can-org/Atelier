c8c359ae744e7bc6e9b4b4b92e77ca0d4f856dc2
Eduardo Nava <ed.nava.valencia@gmail.com>
2025-08-19 16:00:40 -0700

    fix: ip should be 0.0.0.0 iinstead of default localhost for prod

--- server/index.js
+++ server/index.js
@@ -34,5 +34,5 @@ app.all("/*", (req, res) => {
 })
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT, '0.0.0.0');
 console.log(`Server listening at http://localhost:${PORT}`);
